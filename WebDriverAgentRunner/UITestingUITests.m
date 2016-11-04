efe716fdeaf2a1ab3faa9cb7eaf82daface04d79
ymin <yi@zen.ly>
2016-11-04 13:57:38 +0100

    Show ip address before running testRunner

--- WebDriverAgentRunner/UITestingUITests.m
+++ WebDriverAgentRunner/UITestingUITests.m
@@ -14,16 +14,30 @@
 #import <WebDriverAgentLib/FBFailureProofTestCase.h>
 #import <WebDriverAgentLib/FBWebServer.h>
 #import <WebDriverAgentLib/XCTestCase.h>
+#import "FBLogger.h"
+#import "XCUIDevice+FBHelpers.h"
+
+static NSString *const FBServerURLBeginMarker = @"ServerURLHere->";
+static NSString *const FBServerURLEndMarker = @"<-ServerURLHere";
 
 @interface UITestingUITests : FBFailureProofTestCase
 @end
 
 @implementation UITestingUITests
 
+
 + (void)setUp
 {
   [FBDebugLogDelegateDecorator decorateXCTestLogger];
   [super setUp];
+}
+
+/**
+ Given back ip address before run testRunner, fix for one of our device which doesn't show ip in logs 
+ */
+- (void)testIpAddress
+{
+ [FBLogger logFmt:@"%@http://%@:8100%@", FBServerURLBeginMarker, [XCUIDevice sharedDevice].fb_wifiIPAddress, FBServerURLEndMarker];
 }
 
 /**
